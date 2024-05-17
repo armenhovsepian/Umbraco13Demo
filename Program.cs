2f7c8a9e3360a46f50362732a28a209b677f9ffd
Armen <armen@legaldesk.dk>
2024-05-17 09:28:37 +0200

    Umbraco Identity

---
+++ Program.cs
@@ -0,0 +1,34 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
+
+builder.Services.AddControllersWithViews();
+
+
+//builder.Services.AddScoped(typeof(UserManager<MemberIdentityUser>));
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+app.UseHttpsRedirection();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
